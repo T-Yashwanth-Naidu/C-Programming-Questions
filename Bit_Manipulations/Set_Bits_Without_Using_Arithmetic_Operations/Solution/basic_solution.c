5a33f217f7ab944323ac8636b79e22cadf8921b4
T-Yashwanth-Naidu <yashwanthtikkisetty@gmail.com>
2026-05-02 17:53:10 -0400

    add basic sol for Set bits

---
+++ Bit_Manipulations/Set_Bits_Without_Using_Arithmetic_Operations/Solution/basic_solution.c
@@ -0,0 +1,37 @@
+/*
+
+Very Basic Solution
+
+@Author: Yashwanth Naidu Tikksietty (https://github.com/T-Yashwanth-Naidu)
+
+*/
+
+#include<stdio.h>
+#include<stdint.h>
+
+void print_binary(uint8_t value){
+
+    for(int i=7;i>=0;i--){
+        printf("%d",((value>>i)&1));
+        if(i%8==0) printf(" ");
+    }printf("\n");
+
+}
+
+int main(){
+
+    uint8_t value = 0b10101011;
+    uint8_t set_n_bits = 4;
+    uint8_t mask = 0xFF;
+
+    printf("Before Setting: ");
+    print_binary(value);
+
+    value = (mask<<set_n_bits);
+      
+    
+    printf("After Setting: ");
+    print_binary(value);
+
+    return 0;
+}
